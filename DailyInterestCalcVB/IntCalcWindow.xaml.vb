0313ebe39fefc10b506dcff49702a69b4f59b316
ComputingUSA <peter.ungsunan@gmail.com>
2021-01-05 15:45:39 -0500

    Interest Calculator ported to Visual Basic

--- DailyInterestCalcVB/IntCalcWindow.xaml.vb
+++ DailyInterestCalcVB/IntCalcWindow.xaml.vb
@@ -1,3 +1,19 @@
-﻿Class MainWindow
+﻿Class IntCalcWindow
+    Private Sub TextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
 
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Dim result As Decimal
+        Dim p As Decimal = Decimal.Parse(pInput.Text)
+        Dim i As Decimal = Decimal.Parse(iInput.Text)
+        Dim days As Decimal = Decimal.Parse(dInput.Text)
+
+        result = p * i / 100 * days / 365
+
+
+        resultDisplay.Text = "Principal: $" & p & vbCrLf & "Interest Rate: " & i & "%" & vbCrLf & "Days: " & days & vbCrLf &
+            "Calculated Interest is $" & Format(result, "##,##0.00")
+        REM MessageBox.Show("Interest is $" & result.ToString)
+    End Sub
 End Class
