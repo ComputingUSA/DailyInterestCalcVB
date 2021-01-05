e9183b3b6f54e49fe4a2f0c540dd2ded95cd9f99
ComputingUSA <peter.ungsunan@gmail.com>
2021-01-05 15:57:23 -0500

    added error handling

--- DailyInterestCalcVB/IntCalcWindow.xaml.vb
+++ DailyInterestCalcVB/IntCalcWindow.xaml.vb
@@ -5,15 +5,16 @@
 
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
         Dim result As Decimal
-        Dim p As Decimal = Decimal.Parse(pInput.Text)
-        Dim i As Decimal = Decimal.Parse(iInput.Text)
-        Dim days As Decimal = Decimal.Parse(dInput.Text)
-
-        result = p * i / 100 * days / 365
-
-
-        resultDisplay.Text = "Principal: $" & p & vbCrLf & "Interest Rate: " & i & "%" & vbCrLf & "Days: " & days & vbCrLf &
+        Try
+            'Catch invalid inputs.
+            Dim p As Decimal = Decimal.Parse(pInput.Text)
+            Dim i As Decimal = Decimal.Parse(iInput.Text)
+            Dim days As Decimal = Decimal.Parse(dInput.Text)
+            result = p * i / 100 * days / 365
+            resultDisplay.Text = "Principal: $" & p & vbCrLf & "Interest Rate: " & i & "%" & vbCrLf & "Days: " & days & vbCrLf &
             "Calculated Interest is $" & Format(result, "##,##0.00")
-        REM MessageBox.Show("Interest is $" & result.ToString)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
     End Sub
 End Class
